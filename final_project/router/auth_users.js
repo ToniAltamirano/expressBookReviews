2987b221d1c0b04701ae96d5fa606bbafd27ab04
Toni A <34126522+ToniAltamirano@users.noreply.github.com>
2023-10-26 23:42:12 +0200

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -4,77 +4,90 @@ let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
+const SECRET_KEY = "fingerprint_customer";
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+  //write code to check is the username is valid
+  return users.some(u => u.username === username);
+
 }
 
-const authenticatedUser = (username,password)=>{
-    let validusers = users.filter((user)=>{
-      return (user.username === username && user.password === password)
-    });
-    if(validusers.length > 0){
-      return true;
-    } else {
-      return false;
-    }
-  }
+const authenticatedUser = (username, password) => { //returns boolean
+  //write code to check if username and password match the one we have in records.
+  const user = users.find(u => u.username === username);
+  return user && user.password === password;
+}
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-  
-    if (!username || !password) {
-        return res.status(404).json({message: "Error logging in"});
-    }
-   if (authenticatedUser(username,password)) {
-      let accessToken = jwt.sign({
-        data: password
-      }, 'access', { expiresIn: 60 * 60 });
-  
-      req.session.authorization = {
-        accessToken,username
-    }
-    return res.status(200).send("User successfully logged in");
-    } else {
-      return res.status(208).json({message: "Invalid Login. Check username and password"});
-    }
-});
+regd_users.post("/login", (req, res) => {
+  //Write your code here
+  const { username, password } = req.body;
 
+
+  if (!isValid(username) || !authenticatedUser(username, password)) {
+    return res.status(401).json({ message: "Invalid username or password" });
+  }
+
+  const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' });
+  users.find(u => u.username === username).token = token;
+  console.log(users);
+  return res.status(200).json({ token });
+});
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  const review = req.params.isbn;
-    let book = books[review]
-    if (book) { //Check is friend exists
-        let DOB = req.body.DOB;
-        //Add similarly for firstName
-        //Add similarly for lastName
-        //if DOB the DOB has been changed, update the DOB 
-        if(DOB) {
-            book["DOB"] = DOB
-        }
-        //Add similarly for firstName
-        //Add similarly for lastName
-        books[review]=book;
-        res.send(`Book with the review ${review} updated.`);
+  const isbn = req.params.isbn;
+  const review = req.body.review;
+  const token = req.headers.authorization.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const username = decoded.username;
+    if (!books[isbn]) {
+      return res.status(404).json({ message: "Book not found" });
     }
-    else{
-        res.send("Unable to find book!");
+
+    if (!books[isbn].reviews) {
+      books[isbn].reviews = [];
     }
+
+    const bookReviews = books[isbn].reviews;
+    const userReview = Object.keys(bookReviews).find(r => r.username === username);
+    if (userReview) {
+      userReview.review = review;
+    } else {
+      books[isbn].reviews[username] = review;
+    }
+    console.log(books[isbn].reviews);
+    return res.status(200).json({ message: "Review added/updated successfully" });
+  } catch (error) {
+    return res.status(401).json({ message: error.message });
+  }
 });
 
 regd_users.delete("/auth/review/:isbn", (req, res) => {
-    const usern = req.session.username
-    const isbn = req.params.isbn;
-    let book = books[isbn]
-    if (book && usern === book.user){
-        delete friends[email]
+  const isbn = req.params.isbn;
+  const token = req.headers.authorization.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const username = decoded.username;
+
+    if (!books[isbn]) {
+      return res.status(404).json({ message: "Book not found" });
     }
-    res.send(`Review of the book ${book} deleted.`);
-  });
+    if (!books[isbn].reviews) {
+      return res.status(404).json({ message: "No reviews found for this book" });
+    }
+
+    books[isbn].reviews = Object.keys(books[isbn].reviews).find(r => r.username !== username);
+    return res.status(200).json({ message: "Review deleted successfully" });
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
